039c4b8640dafa26d8450d502ebd470256bfb275
Luke1836 <codingluke2023@gmail.com>
2024-10-13 19:16:12 +0530

    Deployment 6

--- vite-project/src/firebase.js
+++ vite-project/src/firebase.js
@@ -3,6 +3,8 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 const  API_BASE_URL = import.meta.env.VITE_API_KEY;
 const  AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
 const  PROJECTID = import.meta.env.VITE_PROJECTID;
@@ -10,7 +12,7 @@ const  STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
 const  MESSAGING_SENDERID = import.meta.env.VITE_MESSAGING_SENDERID;
 const APPID = import.meta.env.VITE_APPID;
 const MEASUREMENTID = import.meta.env.VITE_MEASUREMENTID;
-import { useAuthState } from 'react-firebase-hooks/auth';
+
 
 
 const app = initializeApp({
