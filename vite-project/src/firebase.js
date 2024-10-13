7f7a1dd9130c891c8683acad336292dcdc5b83c9
Luke1836 <codingluke2023@gmail.com>
2024-10-13 23:24:48 +0530

    Deployment 1

--- vite-project/src/firebase.js
+++ vite-project/src/firebase.js
@@ -1,20 +1,17 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { useAuthState } from 'react-firebase-hooks/auth';
 
-const  API_BASE_URL = import.meta.env.VITE_API_KEY;
-const  AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
-const  PROJECTID = import.meta.env.VITE_PROJECTID;
-const  STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
-const  MESSAGING_SENDERID = import.meta.env.VITE_MESSAGING_SENDERID;
+// Load environment variables
+const API_BASE_URL = import.meta.env.VITE_API_KEY;
+const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
+const PROJECTID = import.meta.env.VITE_PROJECTID;
+const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
+const MESSAGING_SENDERID = import.meta.env.VITE_MESSAGING_SENDERID;
 const APPID = import.meta.env.VITE_APPID;
 const MEASUREMENTID = import.meta.env.VITE_MEASUREMENTID;
 
-
+// Initialize Firebase
 const app = initializeApp({
     apiKey: API_BASE_URL,
     authDomain: AUTH_DOMAIN,
@@ -22,9 +19,10 @@ const app = initializeApp({
     storageBucket: STORAGE_BUCKET,
     messagingSenderId: MESSAGING_SENDERID,
     appId: APPID,
-    measurementId: MEASUREMENTID
-  });
+    measurementId: MEASUREMENTID,
+});
 
-export const auth = getAuth();
+// Initialize Firebase services
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
