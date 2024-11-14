5d89c87721e58a6bbe7708caf4c01257c4974966
Luke1836 <codingluke2023@gmail.com>
2024-11-14 20:58:56 +0530

    Cart component added

--- vite-project/src/Components/Cart.jsx
+++ vite-project/src/Components/Cart.jsx
@@ -4,7 +4,14 @@ export default function Cart()
 {
     return (
         <section className="bg-[#180101] h-screen w-screen relative">
-            
+            <div className="">
+                <h1 className="">
+
+                </h1>
+            </div>
+            <div className="">
+                
+            </div>
         </section>
         
     )
